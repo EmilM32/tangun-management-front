c24af245e2dcf85983e3f34e1e8b829e7a78c0bd
Emil Majer <emajer32@gmail.com>
2020-03-08 13:58:16 +0100

    add stylus style

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <template>
   <v-app>
     <v-content>
-      <v-card color='primary'>
-        test
+      <v-card color="primary">
+        <v-btn
+          color="secondary"
+          @click='changeTheme'
+          fab icon
+        >
+          <v-icon>mdi-account</v-icon>
+        </v-btn>
       </v-card>
     </v-content>
   </v-app>
@@ -11,7 +17,20 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 @Component
-export default class Main extends Vue {
+export default class App extends Vue {
+  get isDark () {
+    return this.$vuetify.theme.dark
+  }
 
+  set isDark (value) {
+    this.$vuetify.theme.dark = value
+  }
+
+  changeTheme (): void {
+    this.isDark = !this.isDark
+  }
 }
 </script>
+<style lang="stylus">
+@import '/themes/style.styl'
+</style>
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -2,29 +2,18 @@ import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
 import { Colors } from '@/themes/colors'
 
+const { light, dark } = Colors
+
 Vue.use(Vuetify)
 
 export default new Vuetify({
   theme: {
     themes: {
-      light: {
-        primary: Colors.light.primary,
-        secondary: Colors.light.secondary,
-        accent: Colors.light.accent,
-        error: Colors.light.error,
-        info: Colors.light.info,
-        success: Colors.light.success,
-        warning: Colors.light.warning
-      },
-      dark: {
-        primary: Colors.dark.primary,
-        secondary: Colors.dark.secondary,
-        accent: Colors.dark.accent,
-        error: Colors.dark.error,
-        info: Colors.dark.info,
-        success: Colors.dark.success,
-        warning: Colors.dark.warning
-      }
+      light: light,
+      dark: dark
     }
+  },
+  icons: {
+    iconfont: 'mdiSvg' // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4' || 'faSvg'
   }
 })
